3decb997c7d1fe744ef295adb9d1bd06274f7ad9
thebladehit <boqdanyarmolka@gmail.com>
2024-02-15 21:57:53 +0200

    Implemented getTimeHandler

--- server.go
+++ server.go
@@ -1,19 +1,23 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
 )
 
-func getTimeHandler(w http.ResponseWriter, r *http.Request) {
-    // TODO : implement function for getting current time
+func getTimeHandler(w http.ResponseWriter, _ *http.Request) {
+	curTime := map[string]string{"time": time.Now().Format(time.RFC3339)}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(curTime)
 }
 
 func main() {
-    http.HandleFunc("/time", getTimeHandler)
-    fmt.Println("Server is running on http://localhost:8795")
-    if err := http.ListenAndServe(":8795", nil); err != nil {
-        log.Fatal(err)
-    }
+	http.HandleFunc("/time", getTimeHandler)
+	fmt.Println("Server is running on http://localhost:8795")
+	if err := http.ListenAndServe(":8795", nil); err != nil {
+		log.Fatal(err)
+	}
 }
